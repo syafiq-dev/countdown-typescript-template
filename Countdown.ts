614cf1b0c9e501b796ae73a36da6b1c90dc710c1
Syafiq <95024245+syafiq-dev@users.noreply.github.com>
2026-03-24 15:11:33 +0800

    Update Countdown.ts

--- Countdown.ts
+++ Countdown.ts
@@ -51,8 +51,35 @@ Telegram.sendMessage1.setText(text);
 */
 
 const p2_months = 6
+const p2_days = p2_months * 30
+
+const p2_duration = moment.duration(now.diff(startingDate))
+const p2_durationDays = Math.floor(p2_duration.as('days'))
+
+// Calculate date 44 days before target date
+const p2_endDate = startingDate.add(p2_days, 'days');
+// Format the countdown date
+const p2_lastDate = p2_endDate.format("D MMM YYYY");
+
+var p2_text = `<b><u>Countdown pantang 6 bulan:</u></b>\n
+<code>${p2_days - p2_durationDays} days left</code>\n
+Last Date: ${p2_lastDate}\n
+`
+if(p2_days == p2_durationDays){
+  p2_text = `<b><u>Countdown pantang 6 bulan:</u></b>\n
+  <b>Today is the last day!</b>\n
+  Last Date: ${p2_lastDate}\n
+  `
+}
+
+if(p2_durationDays <= p2_days){
+  text += p2_text
+}else{
+  text = 'No pantang'
+  Telegram.sendMessage2.skip(text)
+}
+
+Telegram.sendMessage2.setText(text);
 
 
 // ===============
-
-Telegram.sendMessage2.skip('in progress')
